2e3c58a1608ed4044a0af11f98b7b8440ce7ca6d
Nikolay Kovalev <47989616+NikkoDevelop@users.noreply.github.com>
2024-04-23 02:04:47 +0300

    Initial commit

---
+++ src/HealthcheckController.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+import { version } from '../package.json';
+
+@Controller()
+export class HealthcheckController {
+  @Get('/')
+  public async index(): Promise<string> {
+    return Promise.resolve(`Nest template ${version} ready`);
+  }
+
+  @Get('/check/ready')
+  public async ready(): Promise<string> {
+    return Promise.resolve(`Nest template ${version} ready`);
+  }
+}
